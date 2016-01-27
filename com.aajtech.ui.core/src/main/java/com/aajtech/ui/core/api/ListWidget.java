5ef315bd821d8470ad9825e5c912922753752a50
Lautaro Brasseur <lbrasseur@gmail.com>
2016-01-26 23:41:33 -0300

    Vaadin list widget WIP

--- com.aajtech.ui.core/src/main/java/com/aajtech/ui/core/api/ListWidget.java
+++ com.aajtech.ui.core/src/main/java/com/aajtech/ui/core/api/ListWidget.java
@@ -1,7 +1,5 @@
 package com.aajtech.ui.core.api;
 
-import java.util.List;
-
-public interface ListWidget extends ValueWidget<List<Widget>> {
+public interface ListWidget extends ValueWidget<Iterable<Widget>> {
 
 }
--- com.aajtech.ui.vaadin/src/main/java/com/aajtech/ui/vaadin/widget/VaadinListWidget.java
+++ com.aajtech.ui.vaadin/src/main/java/com/aajtech/ui/vaadin/widget/VaadinListWidget.java
@@ -1,37 +1,34 @@
 package com.aajtech.ui.vaadin.widget;
 
-import java.util.List;
-
 import com.aajtech.model.core.api.Type;
 import com.aajtech.model.core.api.Value;
 import com.aajtech.model.core.impl.BaseValue;
 import com.aajtech.model.core.impl.java.JavaType;
-import com.aajtech.model.core.impl.java.TypeLiteral;
 import com.aajtech.ui.core.api.ListWidget;
 import com.aajtech.ui.core.api.Widget;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.VerticalLayout;
 
 public class VaadinListWidget extends VaadinWidget<VerticalLayout>implements ListWidget {
-	private final Value<List<Widget>> value;
+	private final Value<Iterable<Widget>> value;
 
 	public VaadinListWidget() {
 		super(new VerticalLayout());
-		value = new BaseValue<List<Widget>>() {
-			private List<Widget> value;
+		value = new BaseValue<Iterable<Widget>>() {
+			private Iterable<Widget> value;
 
 			@Override
-			public List<Widget> get() {
+			public Iterable<Widget> get() {
 				return value;
 			}
 
 			@Override
-			public Type<List<Widget>> getType() {
-				return JavaType.of(new TypeLiteral<List<Widget>>(){});
+			public Type<Iterable<Widget>> getType() {
+				return JavaType.ofIterable(Widget.class);
 			}
 
 			@Override
-			protected void setValue(List<Widget> value) {
+			protected void setValue(Iterable<Widget> value) {
 				this.value = value;
 				component.removeAllComponents();
 				for (Widget widget : value) {
@@ -42,7 +39,7 @@ public class VaadinListWidget extends VaadinWidget<VerticalLayout>implements Lis
 	}
 
 	@Override
-	public Value<List<Widget>> getValue() {
+	public Value<Iterable<Widget>> getValue() {
 		return value;
 	}
 }
