1d9469da07601e16cc61b9760db6e2a4aca17376
Lautaro Brasseur <lbrasseur@gmail.com>
2016-02-22 16:50:30 -0300

    Moving i18n from Nibiru Mobile to AAJ UI

---
+++ com.aajtech.ui.core/src/main/java/com/aajtech/ui/core/impl/i18n/MessageInvocationHandler.java
@@ -0,0 +1,28 @@
+package com.aajtech.ui.core.impl.i18n;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+import javax.annotation.Nullable;
+
+public class MessageInvocationHandler implements InvocationHandler {
+	private final ResourceBundle resourceBundle;
+
+	public MessageInvocationHandler(Class<?> proxyClass) {
+		checkNotNull(proxyClass);
+		resourceBundle = ResourceBundle.getBundle(proxyClass.getName());
+	}
+
+	@Override
+	public Object invoke(@Nullable Object proxy, Method method,
+			@Nullable Object[] args) throws Throwable {
+		checkNotNull(method);
+		String message = resourceBundle.getString(method.getName());
+		MessageFormat messageFormat = new MessageFormat(message);
+		return messageFormat.format(args);
+	}
+}
---
+++ com.aajtech.ui.core/src/main/java/com/aajtech/ui/core/impl/i18n/MessageProvider.java
@@ -0,0 +1,23 @@
+package com.aajtech.ui.core.impl.i18n;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.lang.reflect.Proxy;
+
+import javax.inject.Provider;
+
+public class MessageProvider<T> implements Provider<T> {
+	private final Class<T> proxyClass;
+
+	public MessageProvider(Class<T> proxyClass) {
+		this.proxyClass = checkNotNull(proxyClass);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get() {
+		return (T) Proxy.newProxyInstance(proxyClass.getClassLoader(),
+				new Class<?>[] { proxyClass }, new MessageInvocationHandler(
+						proxyClass));
+	}
+}
