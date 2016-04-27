3d24eb7e67e4fc2cb9d061babfb55d48f88bdf3c
Lautaro Brasseur <lbrasseur@gmail.com>
2016-04-27 15:24:06 -0300

    Button focusable set to false

--- com.aajtech.ui.android/src/main/java/com/aajtech/ui/android/widget/AndroidButton.java
+++ com.aajtech.ui.android/src/main/java/com/aajtech/ui/android/widget/AndroidButton.java
@@ -10,10 +10,19 @@ import android.content.Context;
 public class AndroidButton extends AndroidLabel implements Button {
 	@Inject
 	public AndroidButton(Context context, StyleResolver styleResolver) {
-		this(new android.widget.Button(context), styleResolver);
+		super(context, styleResolver);
 	}
 
 	public AndroidButton(android.widget.Button button, StyleResolver styleResolver) {
 		super(button, styleResolver);
+	}
+
+	@Override
+	android.widget.Button buildControl(Context context, int styleResource) {
+		android.widget.Button button = styleResource == 0 ? new android.widget.Button(context) : new android.widget.Button(context, null, styleResource);
+		// Set focusable to false so it can be added to lists.
+		// Click event on lists fails randomly if the widget is focusable. 
+		button.setFocusable(false);
+		return button;
 	}
 }
