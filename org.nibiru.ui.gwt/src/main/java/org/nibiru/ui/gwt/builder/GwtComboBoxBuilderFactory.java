572b08406f24abc79f74a73967dc3140d4b64368
Lautaro Brasseur <lbrasseur@gmail.com>
2016-11-03 09:59:54 -0300

    Common bindings for combo and radio button builders

--- org.nibiru.ui.gwt/src/main/java/org/nibiru/ui/gwt/builder/GwtComboBoxBuilderFactory.java
+++ org.nibiru.ui.gwt/src/main/java/org/nibiru/ui/gwt/builder/GwtComboBoxBuilderFactory.java
@@ -4,10 +4,15 @@ import org.nibiru.ui.core.impl.builder.ComboBoxBuilder;
 import org.nibiru.ui.core.impl.builder.ComboBoxBuilderFactory;
 import org.nibiru.ui.gwt.widget.GwtComboBox;
 
-public class GwtComboBoxBuilderFactory implements ComboBoxBuilderFactory{
-	@Override
-	public <T> ComboBoxBuilder<T> create(Class<T> valueClass) {
-		return new ComboBoxBuilder<T>(new GwtComboBox<T>());
-	}
+import javax.inject.Inject;
 
+public class GwtComboBoxBuilderFactory implements ComboBoxBuilderFactory {
+    @Inject
+    public GwtComboBoxBuilderFactory() {
+    }
+
+    @Override
+    public <T> ComboBoxBuilder<T> create(Class<T> valueClass) {
+        return new ComboBoxBuilder<T>(new GwtComboBox<T>());
+    }
 }
