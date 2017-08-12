6664ae02b1a855d0299bb1493e4d71790d5ac61e
Lautaro Brasseur <lbrasseur@gmail.com>
2017-08-12 20:27:58 -0300

    Color fixes

--- org.nibiru.ui.core/src/main/java/org/nibiru/ui/core/api/style/Color.java
+++ org.nibiru.ui.core/src/main/java/org/nibiru/ui/core/api/style/Color.java
@@ -1,45 +1,48 @@
 package org.nibiru.ui.core.api.style;
 
 public class Color {
-	public static final Color TRANSPARENT = new Color(0, 0, 0, 0);
-	public static final Color WHITE = new Color(255, 255, 255);
-	public static final Color BLACK = new Color(0, 0, 0);
-	public static final Color RED = new Color(255, 0, 0);
-	public static final Color GREEN = new Color(0, 255, 0);
-	public static final Color BLUE = new Color(255, 0, 0);
-	private final int red;
-	private final int green;
-	private final int blue;
-	private final int alpha;
+    public static final Color TRANSPARENT = new Color(0, 0, 0, 0);
+    public static final Color WHITE = new Color(255, 255, 255);
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color RED = new Color(255, 0, 0);
+    public static final Color GREEN = new Color(0, 255, 0);
+    public static final Color BLUE = new Color(0, 0, 255);
+    public static final Color CYAN = new Color(0, 255, 255);
+    public static final Color MAGENTA = new Color(255, 0, 255);
+    public static final Color YELLOW = new Color(255, 255, 0);
+    private final int red;
+    private final int green;
+    private final int blue;
+    private final int alpha;
 
-	public Color(int red, int green, int blue) {
-		this(red, green, blue, 255);
-	}
+    public Color(int red, int green, int blue) {
+        this(red, green, blue, 255);
+    }
 
-	public Color(int red, int green, int blue, int alpha) {
-		this.red = red;
-		this.green = green;
-		this.blue = blue;
-		this.alpha = alpha;
-	}
+    public Color(int red, int green, int blue, int alpha) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+    }
 
-	public int getRed() {
-		return red;
-	}
+    public int getRed() {
+        return red;
+    }
 
-	public int getGreen() {
-		return green;
-	}
+    public int getGreen() {
+        return green;
+    }
 
-	public int getBlue() {
-		return blue;
-	}
+    public int getBlue() {
+        return blue;
+    }
 
-	public int getAlpha() {
-		return alpha;
-	}
+    public int getAlpha() {
+        return alpha;
+    }
 
-	public String asCss() {
-		return "rgba(" + red + ", " + green + ", " + blue + ", " + alpha / 255f + ")";
-	}
+    public String asCss() {
+        return "rgba(" + red + ", " + green + ", " + blue + ", " + alpha / 255f + ")";
+    }
 }
