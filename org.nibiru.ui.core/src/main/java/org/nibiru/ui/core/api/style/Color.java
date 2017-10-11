a1aad7c59dfedb8de339a4866eec4c6aef7cc01d
Lautaro Brasseur <lbrasseur@gmail.com>
2017-10-11 16:51:44 -0300

    Color improvements

--- org.nibiru.ui.core/src/main/java/org/nibiru/ui/core/api/style/Color.java
+++ org.nibiru.ui.core/src/main/java/org/nibiru/ui/core/api/style/Color.java
@@ -1,15 +1,68 @@
 package org.nibiru.ui.core.api.style;
 
 public class Color {
+    private static final int NON_ALPHA_SPEC_LENGHT = 7;
+    private static final int ALPHA_SPEC_LENGHT = NON_ALPHA_SPEC_LENGHT + 2;
+
+    public static Color fromString(String color) {
+        if (color.startsWith("#")) {
+            if (color.length() == NON_ALPHA_SPEC_LENGHT
+                    || color.length() == ALPHA_SPEC_LENGHT) {
+                int red = parseColor(color, 1);
+                int green = parseColor(color, 3);
+                int blue = parseColor(color, 5);
+                if (color.length() == ALPHA_SPEC_LENGHT) {
+                    int alpha = parseColor(color, 7);
+                    return new Color(red, green, blue, alpha);
+                } else {
+                    return new Color(red, green, blue);
+                }
+            }
+        } else {
+            switch (color.toUpperCase()) {
+                case "TRANSPARENT":
+                    return TRANSPARENT;
+                case "WHITE":
+                    return WHITE;
+                case "BLACK":
+                    return BLACK;
+                case "RED":
+                    return RED;
+                case "GREEN":
+                    return GREEN;
+                case "BLUE":
+                    return BLUE;
+                case "CYAN":
+                    return CYAN;
+                case "MAGENTA":
+                    return MAGENTA;
+                case "YELLOW":
+                    return YELLOW;
+                case "GRAY":
+                    return GRAY;
+                case "LIGHT_GRAY":
+                    return LIGHT_GRAY;
+            }
+        }
+        throw new IllegalArgumentException("Invalid color: " + color);
+    }
+
+    private static int parseColor(String spec, int start) {
+        return Integer.parseInt(spec.substring(start, start + 2), 16);
+    }
+
     public static final Color TRANSPARENT = new Color(0, 0, 0, 0);
-    public static final Color WHITE = new Color(255, 255, 255);
+    public static final Color WHITE = new Color(0xFF, 0xFF, 0xFF);
     public static final Color BLACK = new Color(0, 0, 0);
-    public static final Color RED = new Color(255, 0, 0);
-    public static final Color GREEN = new Color(0, 255, 0);
-    public static final Color BLUE = new Color(0, 0, 255);
-    public static final Color CYAN = new Color(0, 255, 255);
-    public static final Color MAGENTA = new Color(255, 0, 255);
-    public static final Color YELLOW = new Color(255, 255, 0);
+    public static final Color RED = new Color(0xFF, 0, 0);
+    public static final Color GREEN = new Color(0, 0xFF, 0);
+    public static final Color BLUE = new Color(0, 0, 0xFF);
+    public static final Color CYAN = new Color(0, 0xFF, 0xFF);
+    public static final Color MAGENTA = new Color(0xFF, 0, 0xFF);
+    public static final Color YELLOW = new Color(0xFF, 0xFF, 0);
+    public static final Color GRAY = new Color(0x80, 0x80, 0x80);
+    public static final Color LIGHT_GRAY = new Color(0xD3, 0xD3, 0xD3);
+
     private final int red;
     private final int green;
     private final int blue;
