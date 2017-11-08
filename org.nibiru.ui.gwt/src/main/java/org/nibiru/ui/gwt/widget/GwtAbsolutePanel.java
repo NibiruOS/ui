ef89b80d7a728e4d2099c834e537b80f66d2c447
Lautaro Brasseur <lbrasseur@gmail.com>
2017-11-08 11:57:11 -0300

    GWT absoulte panel positioning fixes

--- org.nibiru.ui.gwt/src/main/java/org/nibiru/ui/gwt/widget/GwtAbsolutePanel.java
+++ org.nibiru.ui.gwt/src/main/java/org/nibiru/ui/gwt/widget/GwtAbsolutePanel.java
@@ -1,55 +1,74 @@
 package org.nibiru.ui.gwt.widget;
 
 
+import com.google.common.collect.Maps;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 
 import org.nibiru.ui.core.api.Widget;
+
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class GwtAbsolutePanel extends GwtContainer<AbsolutePanel> implements org.nibiru.ui.core.api.AbsolutePanel {
-	@Inject
-	public GwtAbsolutePanel() {
-		this(new AbsolutePanel());
-	}
+    private static Map<Widget, Position> positions;
 
-	public GwtAbsolutePanel(AbsolutePanel panel) {
-		super(panel);
-	}
+    @Inject
+    public GwtAbsolutePanel() {
+        this(new AbsolutePanel());
+    }
 
-	@Override
-	public Position getPosition(Widget child) {
-		checkNotNull(child);
-		final com.google.gwt.user.client.ui.Widget gwtWidget = (com.google.gwt.user.client.ui.Widget) child.asNative();
-		return new Position() {
+    public GwtAbsolutePanel(AbsolutePanel panel) {
+        super(panel);
+        positions = Maps.newHashMap();
+    }
 
-			@Override
-			public int getX() {
-				return control.getWidgetLeft(gwtWidget);
-			}
+    @Override
+    public Position getPosition(Widget child) {
+        checkNotNull(child);
+        return positions.computeIfAbsent(child,
+                (key) -> new GwtPosition(key));
+    }
 
-			@Override
-			public Position setX(int x) {
-				control.setWidgetPosition(gwtWidget, x, getY());
-				return this;
-			}
+    @Override
+    public void requestLayout() {
+    }
 
-			@Override
-			public int getY() {
-				return control.getWidgetTop(gwtWidget);
-			}
+    // This class caches X and Y coordinates because getWidgetLeft()
+    // and getWidgetTop() return 0 if the widget is hiden.
+    private class GwtPosition implements Position {
+        private final com.google.gwt.user.client.ui.Widget gwtWidget;
+        private int x;
+        private int y;
 
-			@Override
-			public Position setY(int y) {
-				control.setWidgetPosition(gwtWidget, getX(), y);
-				return this;
-			}
-		};
-	}
+        private GwtPosition(Widget child) {
+            this.gwtWidget = (com.google.gwt.user.client.ui.Widget) child.asNative();
+        }
 
-	@Override
-	public void requestLayout() {
-	}
+        @Override
+        public int getX() {
+            return x;
+        }
+
+        @Override
+        public Position setX(int x) {
+            this.x = x;
+            control.setWidgetPosition(gwtWidget, x, getY());
+            return this;
+        }
+
+        @Override
+        public int getY() {
+            return y;
+        }
+
+        @Override
+        public Position setY(int y) {
+            this.y = y;
+            control.setWidgetPosition(gwtWidget, getX(), y);
+            return this;
+        }
+    }
 }
